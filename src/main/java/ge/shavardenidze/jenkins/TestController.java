9bc70f855a3c5c707e4c8bcc4651734c3e492bc4
shavardenidze <50408233+shavardenidze@users.noreply.github.com>
2021-03-12 12:20:30 +0400

    Initial commit

---
+++ src/main/java/ge/shavardenidze/jenkins/TestController.java
@@ -0,0 +1,16 @@
+package ge.shavardenidze.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "test")
+public class TestController {
+
+    @GetMapping(value = "")
+    public String authorizedUser() {
+        return "Test 12" ;
+    }
+
+}
